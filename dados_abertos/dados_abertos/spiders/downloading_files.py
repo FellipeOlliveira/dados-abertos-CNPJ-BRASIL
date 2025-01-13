8b24a2d3f1032495b8b43daa99dd50345b9160b6
Marcos Fellipe Silva de Oliveira <marcosf1625@gmail.com>
2025-01-13 13:13:10 -0300

    FEAT: Get_Current_Files
    Pegando data dos arquivos a serem baixados

--- dados_abertos/dados_abertos/spiders/downloading_files.py
+++ dados_abertos/dados_abertos/spiders/downloading_files.py
@@ -1,4 +1,8 @@
+from typing import Iterable
+
+from pathlib import Path
 import scrapy
+from scrapy import Request
 
 
 class DownloadingFilesSpider(scrapy.Spider):
@@ -6,5 +10,24 @@ class DownloadingFilesSpider(scrapy.Spider):
     allowed_domains = ["arquivos.receitafederal.gov.br"]
     start_urls = ["https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj"]
 
+    def start_requests(self) -> Iterable[Request]:
+        yield Request(
+            url=self.start_urls[0],
+            callback=self.get_current_files
+        )
+    def get_current_files(self, response):
+        link = response.xpath("//a/@href")[-2]
+
+        link = response.urljoin(str(link))
+
+        return Request(
+            url=link,
+            callback=self.parse
+        )
+
+
+
     def parse(self, response):
-        pass
+        response.xpath("//a[contains(text(), 'Estabelecimentos') or contains(text(), 'Empressas')]/@href")
+
+        
